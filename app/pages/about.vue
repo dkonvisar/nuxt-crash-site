2a667b936e18e1142f5e919ad14dfe545283f63b
denis <denis.konvisar@gmail.com>
2026-03-25 18:41:29 +0200

    removed app.vue & create pages

---
+++ app/pages/about.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>About</h1>
+  <p>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Animi dolores aperiam est nesciunt fuga explicabo illo
+    omnis perspiciatis consectetur dolorum fugiat maxime ipsam ipsum asperiores, minus expedita tenetur rerum magnam.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum iste nemo ipsum libero fugit aliquam blanditiis,
+    deleniti vel voluptatem quasi, voluptates recusandae nulla ea. Excepturi autem laboriosam officiis expedita minus!
+  </p>
+</template>
