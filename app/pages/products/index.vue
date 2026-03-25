9bb036002e28c6588de52fedf511a609aa1b3339
denis <denis.konvisar@gmail.com>
2026-03-25 19:07:25 +0200

    NuxtLink

--- app/pages/products/index.vue
+++ app/pages/products/index.vue
@@ -1,3 +1,12 @@
 <template>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/">Home</NuxtLink></li>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/products">Products</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
   <h1>Products</h1>
 </template>
